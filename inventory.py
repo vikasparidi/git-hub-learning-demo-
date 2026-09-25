bd9fd5d0911ee5f372fc9789b61b9312ed150203
Paridi Vikas <71751311+vikasparidi@users.noreply.github.com>
2026-09-25 23:58:20 +0530

    Correct product list syntax in inventory.py
    Fixed syntax errors in the product list.

--- inventory.py
+++ inventory.py
@@ -3,6 +3,9 @@ products = [
     {"name": "Keyboard", "price": 2000},
     {"name": "Mouse", "price": 1000}
     {"name": "Monitor", "price": 15000}
+  {"name": "Keyboard", "price": 2000},
+    {"name": "Mouse", "price": 1000}
+    {"name": "Monitor", "price": 15000}
 ]
 
 for product in products:
