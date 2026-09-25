7115c5b3b445e17ffc66fad6fc5a1d00d241cb82
Paridi Vikas <71751311+vikasparidi@users.noreply.github.com>
2026-09-25 23:29:28 +0530

    Add basic inventory program

---
+++ inventory.py
@@ -0,0 +1,8 @@
+products = [
+    {"name": "Laptop", "price": 50000},
+    {"name": "Keyboard", "price": 2000},
+    {"name": "Mouse", "price": 1000}
+]
+
+for product in products:
+    print(product["name"], "-", product["price"])
