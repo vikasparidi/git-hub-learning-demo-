8215221b8f530a23de06d947acdd98c5eb3eaf0b
Paridi Vikas <71751311+vikasparidi@users.noreply.github.com>
2026-09-26 00:00:21 +0530

    Correct product list syntax in inventory.py
    Fix syntax error in product list by adding missing commas.

--- inventory.py
+++ inventory.py
@@ -6,6 +6,7 @@ products = [
   {"name": "Keyboard", "price": 2000},
     {"name": "Mouse", "price": 1000}
     {"name": "Monitor", "price": 15000}
+{"name": "Mouse", "price": 1000}
 ]
 
 for product in products:
